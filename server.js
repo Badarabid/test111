b1c747cb6cc2e9c6c72aee68816ffa787eda5400
Badar Abid <mbadar.abid@gmail.com>
2026-03-29 19:48:25 +0500

    pre-pull commit

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('CI/CD is Live!'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get('/', (req, res) => {
+  res.send(`
+    <div style="text-align:center; margin-top:50px; font-family:sans-serif;">
+      <h1 style="color: #4CAF50;">Mubarak Ho! CI/CD Kaam Kar Raha Hai!</h1>
+      <p style="font-size: 20px;">Ye update GitHub Actions ke zariye <b>Automatically</b> deploy hui hai.</p>
+      <p>Current Time: ${new Date().toLocaleTimeString()}</p>
+    </div>
+  `);
+});
